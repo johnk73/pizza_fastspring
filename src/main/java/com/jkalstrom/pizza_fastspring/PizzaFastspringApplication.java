f5ff71b41a1bd164511cca54d6b261797ec41184
johnkalstrom <john.kalstrom@gmail>
2019-01-19 16:54:52 -0800

    Only got empty initialzr (start.spring.io) and mysql schema -- while I love how MySQL Workbench looks, it has some serious issues

---
+++ src/main/java/com/jkalstrom/pizza_fastspring/PizzaFastspringApplication.java
@@ -0,0 +1,14 @@
+package com.jkalstrom.pizza_fastspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaFastspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaFastspringApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jkalstrom/pizza_fastspring/PizzaFastspringApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jkalstrom.pizza_fastspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PizzaFastspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
